a3ab374a8419fcc41ee9d99fac5fe0bc7f781f5c
Mezaque Rodrigues <mezaquerodrigues1@gmail.com>
2020-11-23 14:00:03 -0300

    Inserção do sout("Mezaque") na classe LuisClaudio

--- src/main/LuisClaudio.java
+++ src/main/LuisClaudio.java
@@ -4,8 +4,8 @@ package main;
 public class LuisClaudio {
     
     public static void main(String[] args) {
-        
         System.out.println("Luís Cláudio");
+        System.out.println("Mezaque");
     }
     
 }
