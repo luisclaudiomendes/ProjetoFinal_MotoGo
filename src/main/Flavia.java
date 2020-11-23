4025747825247e68f1d041e2b7f29f70033b0931
Mezaque Rodrigues <mezaquerodrigues1@gmail.com>
2020-11-23 16:00:36 -0300

    Inserção do sout "Mezaque" na classe Flaviua

--- src/main/Flavia.java
+++ src/main/Flavia.java
@@ -1,10 +1,9 @@
 package main;
 
-public class Flavia
-{
-    public static void main(String[] args)
-    {
+public class Flavia {
+    public static void main(String[] args) {
         System.out.println("Flávia");
         System.out.println("Luís Cláudio");
+        System.out.println("Mezaque");
     }
 }
