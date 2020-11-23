b5dbed749c0cff6867fe1d0341b100cb7e6cc7e8
luisc <luis.mendes@alunos.ifsuldeminas.edu.br>
2020-11-23 15:32:27 -0300

    Adição do sout "Luís Cláudio" a classe Gabrielle.

--- src/main/Gabrielle.java
+++ src/main/Gabrielle.java
@@ -12,5 +12,6 @@ package main;
 public class Gabrielle {
     public static void main(String[] args) {
         System.out.println("Gabrielle");
+        System.out.println("Luís Cláudio");
     }
 }
