1ae05f46f5339073d13dea3012343d4c6e788f45
luisc <luis.mendes@alunos.ifsuldeminas.edu.br>
2020-11-23 13:37:56 -0300

    Criação de uma nova classe no projeto, com sout "Luís Cláudio".

--- src/main/LuisClaudio.java
+++ src/main/LuisClaudio.java
@@ -1,9 +1,10 @@
 
 package main;
 
-public class Aluno {
+public class LuisClaudio {
     
-    public static void main(String[] args) {       
+    public static void main(String[] args) {
+        
         System.out.println("Luís Cláudio");
     }
     
