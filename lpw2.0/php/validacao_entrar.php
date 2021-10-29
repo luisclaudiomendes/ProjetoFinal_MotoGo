e6ddefb25e6970d2abb9823fca420ed3f8aefd9f
flaviaaparecida <flavia.aparecida@alunos.ifsuldeminas.edu.br>
2021-10-29 13:15:19 -0300

    Validação de Login

--- lpw2.0/php/validacao_entrar.php
+++ lpw2.0/php/validacao_entrar.php
@@ -13,11 +13,15 @@ if (strlen($_POST["senha"]) < 6 || strlen($_POST["senha"]) > 8) {
   $erros .= "O campo 'Senha' deve ter entre 6 e 8 caracteres! <br>";
 }
 
-if (strlen($erros) == 0) {
+if (strlen($erros) == 0 and $_POST["email"] == "adm@gmail.com" and $_POST["senha"] == "123456") {
+  session_start();
+  $_SESSION["email"] = $_POST["email"];
   echo "Login efetuado com sucesso! <br>";
   echo "Você será redirecionado para a página de relatórios. <br>";
   header("refresh: 5; url = ../relatorios.html");
 } else {
+  if($_POST["email"] != "adm@gmail.com" or $_POST["senha"] != "123456")
+    $erros .= 'E-mail e/ou senha inválidos! <br>';
   echo $erros . "<br>";
   echo "Você será redirecionado para a página de login. <br>";
   header("refresh: 5; url = ../login.html");
--- lpw2.0/relatorios.php
+++ lpw2.0/relatorios.php
@@ -1,3 +1,10 @@
+<?php
+  session_start();
+  if(!isset($_POST["email"])){
+    header("Location: login.html");
+  }
+?>
+
 <!DOCTYPE HTML>
 <html lang="pt-br">
 
